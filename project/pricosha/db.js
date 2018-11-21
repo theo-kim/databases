701bbf70fffdd413c3e88d6384fafd048ee4eef5
Theodore Kim <tk1931@nyu.edu>
2018-11-21 12:05:35 -0500

    Initial Pricosha Commit

---
+++ project/pricosha/db.js
@@ -0,0 +1,31 @@
+// Import mysql interface
+const mysql = require("mysql");
+
+// Create connection to MAMP server
+const connection = mysql.createConnection({
+	host : "localhost",
+	port : "8889",
+	user: "root",
+	password : "root",
+	database : "pricosha"
+});
+
+// Define easy abstraction for making sql queries
+const query = function (query, values) {
+	return new Promise((resolve, reject) => {
+		if (!values && query.includes("?"))
+			reject("Your query includes placeholders, but you don't provide binding values");
+		connection.query({
+			sql: query,
+			values: values
+		}, function (error, results, fields) {
+			if (error) reject(error);
+			else {
+				resolve(results, fields);
+			}
+		})
+	})
+}
+
+// export module
+module.exports = query;
---
+++ project/pricosha/index.js
@@ -0,0 +1,118 @@
+// Import Express Server Framework
+var express = require('express');
+
+// Import query abstraction
+const sql = require('./db');
+
+// Import other utilizity modules
+var path = require('path');
+
+// Initialize server object
+var app = express();
+var debug = require('debug')('pricosha');
+var http = require('http');
+
+// Specify the utilized directories: views and public resources (i.e. JS and CSS)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Default routing
+app.get('/', (req, res, next) => {
+	res.render('main');
+});
+
+
+// CODE TAKEN FROM DEFAULT EXPRESS APP BOILERPLATE:
+// https://github.com/express/example
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
